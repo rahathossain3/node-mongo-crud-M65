9c3eca119638de9da49c9f1a4fc9de5366ba3b4d
rahathossain3 <rahathossain210@gmail.com>
2022-04-25 03:53:49 +0600

    catch server side data

--- __steps.js
+++ __steps.js
@@ -6,7 +6,7 @@
  * 1.4. script-dev: nodemon index.js
  * 1.5. create index.js
  * 1.6. use 5 steps  to create a node server
- * 
+ 
  * --------------------------------------
  * Create Atlas Account 
  * --------------------------------------
@@ -15,11 +15,21 @@
  * 3. create user dbUser and password
  * 4. Network Access --> ip address : allow all
  * 5. database > connect > code Copy paste in index.js
+ 
  * -------------------------------------
  * CRUD Operation
  * -------------------------------------
  * 1. node mongodb CRUD > Fundamentals
  * 2. create async run function
+ 
+ * ----------------------------------------------
+ * integrate sending data from client to server
+ * -----------------------------------------------
+ * 1. Client Side : Create form
+ * 2. on submit get from data and create user object
+ * 3. on server: create user POST method to receive data on the backend
+ * 4. on Client side: set fetch with POST , headers, body
+ * 5. Make sure you return a json from the POST API
  * 
  * 
  * 
--- index.js
+++ index.js
@@ -7,17 +7,15 @@ const app = express();
 const port = process.env.PROT || 5000;
 
 
-//use middleware
+//use middleware  { for post important too}
 app.use(cors());
 app.use(express.json());
-
 
 
 // user: dbuser1
 // password: TQ5NJyagnGzgDnfn
 
-//mongodb codes start-------------------------
-
+//mongodb codes start---------------------------------------------------
 
 
 const uri = "mongodb+srv://dbuser1:TQ5NJyagnGzgDnfn@cluster0.mdrpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -30,17 +28,18 @@ async function run() {
         await client.connect();
         const userCollection = client.db("foodExpress").collection("user");
 
-        // post a data------
+        // post a data-----(get data from client side)-***************************************
         app.post('/user', (req, res) => {
-            console.log('post add option add')
-        });
 
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            res.send({ result: 'success' })
+        });
 
 
         /* const user = { name: 'monona nodi', email: 'mohonanodi@gmail.com' };
         const result = await userCollection.insertOne(user);
         console.log(`user inserted  with id ${result.insertedId}`) */
-
     }
     finally {
         // continue database use korle  client.close() use kora jaba na
@@ -53,8 +52,7 @@ async function run() {
 run().catch(console.dir);
 
 
-
-//mongodb codes End-------------------------------
+//mongodb codes End------------------------------------------------------------
 
 
 //4
